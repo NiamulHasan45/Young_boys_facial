f1a76d2cbdce7e89df4aeb1801fc108d48027f17
Niamul.Hasan45 <niamul.rony@gmail.com>
2022-03-26 09:56:08 +0600

    first commit

---
+++ src/Utilities/Product/Product.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Product = () => {
+    return (
+        <div>
+            <h3>This one is for checking.</h3>
+        </div>
+    );
+};
+
+export default Product;
