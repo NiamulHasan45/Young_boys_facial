2e2db0a8d68a0b356c2de1b0934c3bc98d4efb67
Niamul.Hasan45 <niamul.rony@gmail.com>
2022-03-26 18:00:45 +0600

    complete

--- src/App.js
+++ src/App.js
@@ -10,6 +10,21 @@ function App() {
     <div >
       <Header></Header>
       <Shop></Shop>
+      <p>
+      How react works?
+      </p>
+      <p>
+      React is a JavaScript library. It creates user interface with more efficient way. React works in declarative code. We can create component as our wish and modify it the way we like. React use virtual tree to compare new data with the previous one, which makes it a performance based library. 
+      </p>
+      <p>
+        Difference between props and state!
+      </p>
+      <p>
+        Props is used to pass value in component when we pass value with props it stores it as object. We can acces those object from component. We can say props as property. 
+      </p>
+      <p>
+        On the other hand state is used to take action with user interaction with website. whenever user iteract with the website state changed. It is also use to perform operation which are interaction based.
+      </p>
     </div>
   );
 }
